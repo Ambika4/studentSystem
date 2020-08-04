cfde58a096e53aeddb33459fdb161e314355f499
Ambika Kumari <kambika1309@gmail.com>
2020-08-05 03:36:06 +0530

    created form for collecting  student  details

--- routes/api/v1/students.js
+++ routes/api/v1/students.js
@@ -9,7 +9,7 @@ router.post('/:id/update_favouriteSport/',studentController.update);
 //route for deleting  id and passing paramas
 router.delete('/:id',studentController.delete);
 //route for creating student
-router.post('/create',studentController.create);
+router.post('/createStudent',studentController.create);
 //route for get all list of students
 router.get('/',studentController.allstudents);
 module.exports=router;
